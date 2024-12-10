133407a3f0aebcfac051409f601040cb5c4a2ddc
TejaswiniHanumanth <htejaswini.hanu@gmail.com>
2024-12-10 23:05:30 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,46 +1,30 @@
 import os
-import cv2
+import pandas as pd
+from PIL import Image
 from torch.utils.data import Dataset
 from torchvision import transforms
-from load_config import load_config
 
-config = load_config()
-
-class TransactionDataset(Dataset):
-    def __init__(self, video_folder, labels, transform=None, frame_skip=None):
-        self.video_folder = video_folder
-        self.labels = labels
+class RetailDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.img_dir = img_dir
         self.transform = transform
-        self.frame_skip = frame_skip or config['hyperparameters']['frame_skip']
-        self.data = self._extract_frames()
-
-    def _extract_frames(self):
-        data = []
-        for video_name, label in self.labels.items():
-            video_path = os.path.join(self.video_folder, video_name)
-            cap = cv2.VideoCapture(video_path)
-            frame_idx = 0
-
-            while cap.isOpened():
-                ret, frame = cap.read()
-                if not ret:
-                    break
-
-                if frame_idx % self.frame_skip == 0:
-                    data.append((frame, label))
-                frame_idx += 1
-
-            cap.release()
-        return data
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        frame, label = self.data[idx]
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
+        img_path = os.path.join(self.img_dir, self.data.iloc[idx, 0])
+        image = Image.open(img_path).convert("RGB")
+        label_cash = self.data.iloc[idx, 1]
+        label_invoice = self.data.iloc[idx, 2]
+        labels = {"cash_transaction": label_cash, "invoice_provided": label_invoice}
         if self.transform:
-            frame = self.transform(frame)
+            image = self.transform(image)
+        return image, labels
 
-        return frame, label
+def get_transforms():
+    return transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+    ])
